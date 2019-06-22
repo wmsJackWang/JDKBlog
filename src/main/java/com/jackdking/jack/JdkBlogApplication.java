c50b4ed23005245125a74c1e5832964fca51e6bf
wmsJackWang <ncl@1234>
2019-06-22 19:54:08 +0800

    提交描述

---
+++ src/main/java/com/jackdking/jack/JdkBlogApplication.java
@@ -0,0 +1,13 @@
+package com.jackdking.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdkBlogApplication.class, args);
+	}
+
+}
